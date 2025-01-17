199c296cbf0f64cf5b1f3785f9b2a180442369c1
Rahaf <rahaf.alaaeldin@gmail.com>
2025-01-18 00:41:18 +0300

    Revert " version 2"
    This reverts commit 89da6909b3567de56cd480923abf705a1e4da3c6.

--- src/types/User.ts
+++
@@ -1,9 +0,0 @@
-import type { Product } from './Product';
-
-export interface User {
-  uid: string;
-  firstName: string;
-  lastName: string;
-  email: string;
-  cartItems: Product[]; // cartItems is an array of Product[]
-}
